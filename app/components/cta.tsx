0752a54a8a23d0e995baf7e8f5b7af3a32d52ec1
Tenma <tenma@osxt.com>
2026-01-20 19:16:08 +0800

    landing

--- app/components/cta.tsx
+++ app/components/cta.tsx
@@ -1,18 +1,36 @@
+import { AnimatedGridPattern } from "@/components/ui/animated-grid-pattern";
+import { Button } from "@/components/ui/button";
+import Typography from "@/components/ui/typography";
+import { cn } from "@/lib/utils";
 
 
 
 const Cta = () => {
   return (
-    <div className="grid grid-cols-8 w-full rounded-2xl overflow-hidden">
-      {Array.from({ length: 8 }).map((_, index) => (
-        <div key={index} className={`col-span-1 bg-gray-800 border-[0.5px] border-dashed h-24`} />
-      ))}
-      {Array.from({ length: 8 }).map((_, index) => (
-        <div key={index} className={`col-span-1 bg-gray-800 border-[0.5px] border-dashed h-24`} />
-      ))}
-      {Array.from({ length: 8 }).map((_, index) => (
-        <div key={index} className={`col-span-1 bg-gray-800 border-[0.5px] border-dashed h-24`} />
-      ))}
+    <div className="bg-card relative flex h-82 w-full items-center justify-center overflow-hidden rounded-lg border p-20">
+      <AnimatedGridPattern
+        width={112}
+        height={112}
+        x={-5}
+        y={-20}
+        // strokeDasharray={5}
+        numSquares={20}
+        maxOpacity={0.1}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "mask-[radial-gradient(800px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%] h-[200%]",
+          "stroke-muted-foreground/30",
+          "text-primary" // Couleur des carrés animés (utilise currentColor)
+        )}
+      />
+
+      <div className="flex flex-col items-center justify-center">
+        <Typography variant="h2" className="text-center text-[42px] mb-0">Ready to make reviews your unfair advantage?</Typography>
+        <Typography variant="description" className="text-muted-foreground">Plug FiveUp into your stack and turn every customer into social proof – without adding more work to your team.</Typography>
+        <Button className="mt-5 z-10">Commencer maintenant</Button>
+      </div>
 
     </div>
   )
--- components/ui/animated-grid-pattern.tsx
+++ components/ui/animated-grid-pattern.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { motion } from "motion/react"
 import {
   ComponentPropsWithoutRef,
   useCallback,
@@ -8,7 +9,6 @@ import {
   useRef,
   useState,
 } from "react"
-import { motion } from "motion/react"
 
 import { cn } from "@/lib/utils"
 
